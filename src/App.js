c9a11a07b177ade2ecba18429c5467b21705c7ea
daniel <danielemesh@gmail.com>
2018-08-07 12:17:37 +0300

    after eject

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.App}>
         burger
       </div>
     );
